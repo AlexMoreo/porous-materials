b9154f1a523297a1f22f1662ed6a5d1dbd934cf8
Alejandro Moreo <alejandro.moreo@isti.cnr.it>
2025-10-15 18:39:16 +0200

    last update in sw

--- src/compare_plots.py
+++ src/compare_plots.py
@@ -2,22 +2,15 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import os
 import numpy as np
+from tqdm import tqdm
 
 # --- methods and paths ---
 # gas = 'nitrogen'
 gas = 'hydrogen'
 
-# if gas=='nitrogen':
-#     methods = ['baseline', "RF", "RF-PCA12-PCAin12", "ff-128-256-128", "ff-64-128-128-64-PCA12", "ff-64-128-128-64-PCA12-0", "ff-128-256-512-256-128-PCA12-0"]
-#     baseline_path = '../attachements/reverse_from_N2'
-#     methods_path  = f'../results/plots/{gas}'
-# else: # no baseline
-#     methods = ["RF", "ff-128-256-128", "ff-128-256-128-y10"]
-#     baseline_path = None
-#     methods_path  = f'../results/plots/{gas}'
-
 baseline_path = None
-methods = ["R3-3L128", "R3-3L128-PCAZY8", "R3-3L128-PCAZY8-dr"]
+# methods = ["R3-3L128", "R3-3L128-PCAZY8", "R3-3L128-PCAZY8-dr"]
+methods = ['R3-XYZ','R3-XYZ-L0','R3-XY','R3-ZY','R3-Y','R3-Xyz','R3-Xy','R3-zy','R3-y']
 
 
 # --- selected models to display ---
@@ -31,7 +24,7 @@ n_cols = len(methods)
 fig, axes = plt.subplots(n_rows, n_cols, figsize=(3 * n_cols, 2.5 * n_rows))
 
 # --- Cargar y mostrar cada imagen ---
-for i, model_id in enumerate(selected_ids):
+for i, model_id in tqdm(enumerate(selected_ids), desc='generating plots', total=len(selected_ids)):
     for j, method in enumerate(methods):
         if j==0 and baseline_path is not None: #baseline method
             img_path = os.path.join(baseline_path, f"model{model_id:02d}.png")
@@ -52,10 +45,12 @@ for i, model_id in enumerate(selected_ids):
         if j == 0:
             axes[i, j].set_ylabel(f"Model {model_id}", fontsize=12)
 
+path_out = f'../results/{gas}{gas_suffix}_comparison.png'
+print(f'plots generated, saving images in {path_out}')
 plt.tight_layout()
-# plt.show()
-plt.savefig(f'../results/{gas}{gas_suffix}_comparison.png',
+plt.savefig(path_out,
     dpi=300,
     bbox_inches="tight",
     pad_inches=0.1,
 )
+print('[Done!]')
