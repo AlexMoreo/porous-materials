3d40711c534b1fab362c78866b55508d987b7586
Alejandro Moreo <alejandro.moreo@isti.cnr.it>
2025-10-23 16:12:36 +0200

    adding all grids in different png

--- src/compare_plots.py
+++ src/compare_plots.py
@@ -11,58 +11,72 @@ gas = 'hydrogen'
 baseline_path = None
 # methods = ['RF', 'RFy', 'RFxy', 'R3-XYZ', 'R3-XYZ-L0', 'R3-Xyz-L0',
 #            'R3-XY','R3-ZY','R3-Y','R3-Xyz','R3-Xy','R3-zy','R3-y']
-methods = (
-        [f'R3-XY{i}' for i in range(5)]
-           # + [f'R3-ZY{i}' for i in range(5)]
-           # + [f'R3-Y{i}' for i in range(5)]
-         + [f'R3-y{i}' for i in range(5)]
-)
-
+# methods = (
+#         [f'R3-XY{i}' for i in range(5)]
+#            # + [f'R3-ZY{i}' for i in range(5)]
+#            # + [f'R3-Y{i}' for i in range(5)]
+#          + [f'R3-y{i}' for i in range(5)]
+# )
+methods = ['RF', 'R2I1O-Y', 'R2I1O-y', 'R2I1O-XY', 'R3-XYZ', 'R3-XY', 'R3-ZY', 'R3-Y', 'R3-y']
 
 
 # --- selected models to display ---
+all_ids = list(range(1,112))
 # selected_ids = [1, 9, 17, 26, 34, 38, 48, 53, 62, 67, 73, 80, 89, 93, 105]
 # selected_ids = [3,10,35,37,68,70]
-selected_ids = [37,68]
+# selected_ids = [1,2,3,4]
 # selected_ids = list(range(10,40))
 # methods = ['R3-XY']+[f'R3-XY-v{i+1}' for i in range(10)]
 methods_path = '../results/plots'
 gas_suffix = '-Gout'  # gas out representation
 
-# --- Crea figura y subplots ---
-n_rows = len(selected_ids)
-n_cols = len(methods)
-fig, axes = plt.subplots(n_rows, n_cols, figsize=(3 * n_cols, 2.5 * n_rows), constrained_layout=False)
-plt.subplots_adjust(wspace=0.0, hspace=0.0)
+n_ids = len(all_ids)
+batch_size = 15  # creates grids of 20 x n_models
+n_batches = (n_ids//batch_size) + (1 if n_ids%batch_size>1 else 0)
 
-# --- Cargar y mostrar cada imagen ---
-for i, model_id in tqdm(enumerate(selected_ids), desc='generating plots', total=len(selected_ids)):
-    for j, method in enumerate(methods):
-        if j==0 and baseline_path is not None: #baseline method
-            img_path = os.path.join(baseline_path, f"model{model_id:02d}.png")
-        else:
-            img_path = os.path.join(methods_path, method, f"model{model_id}{gas_suffix}.png")
-        if os.path.exists(img_path):
-            img = mpimg.imread(img_path)
-            axes[i, j].imshow(img)
-        else:
-            axes[i, j].text(0.5, 0.5, "No image", ha='center', va='center', fontsize=12)
-        axes[i, j].axis('off')
+for batch_i in range(n_batches):
 
-        # column titles (only first)
-        if i == 0:
-            axes[i, j].set_title(method, fontsize=14, weight='bold')
+    selected_ids = all_ids[(batch_i*batch_size):((batch_i+1)*batch_size)]
 
-        # row labels (only first)
-        if j == 0:
-            axes[i, j].set_ylabel(f"Model {model_id}", fontsize=12)
+    # --- Crea figura y subplots ---
+    n_rows = len(selected_ids)
+    n_cols = len(methods)
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=(3 * n_cols, 2.5 * n_rows), constrained_layout=False)
+    plt.subplots_adjust(wspace=0.0, hspace=0.0)
 
-path_out = f'../results/{gas}{gas_suffix}_var_comparison.png'
-print(f'plots generated, saving images in {path_out}')
-plt.tight_layout(pad=0.)
-plt.savefig(path_out,
-    dpi=300,
-    bbox_inches="tight",
-    pad_inches=0.1,
-)
+    from_, to_ = selected_ids[0], selected_ids[-1]
+
+    # --- Cargar y mostrar cada imagen ---
+    for i, model_id in tqdm(enumerate(selected_ids), desc=f'generating plots {from_}-{to_}', total=len(selected_ids)):
+        for j, method in enumerate(methods):
+            if j==0 and baseline_path is not None: #baseline method
+                img_path = os.path.join(baseline_path, f"model{model_id:02d}.png")
+            else:
+                img_path = os.path.join(methods_path, method, f"model{model_id}{gas_suffix}.png")
+            if os.path.exists(img_path):
+                img = mpimg.imread(img_path)
+                axes[i, j].imshow(img)
+            else:
+                axes[i, j].text(0.5, 0.5, "No image", ha='center', va='center', fontsize=12)
+            axes[i, j].axis('off')
+
+            # column titles (only first)
+            if i == 0:
+                axes[i, j].set_title(method, fontsize=14, weight='bold')
+
+            # row labels (only first)
+            if j == 0:
+                axes[i, j].set_ylabel(f"Model {model_id}", fontsize=12)
+
+
+    path_out = f'../results/{gas}{gas_suffix}_comparison_{from_}-{to_}.png'
+    print(f'plots generated, saving images in {path_out}...', end='')
+    plt.tight_layout(pad=0.)
+    plt.savefig(path_out,
+        dpi=300,
+        bbox_inches="tight",
+        pad_inches=0.1,
+    )
+    print('[Done]')
+
 print('[Done!]')
